eacb1be2c8d1f6391686b79e626d5b5d80e4b66e
hamidraza117-rgb <hamidraza117@gmail.com>
2026-04-21 20:21:40 +0100

    Update silver.py

--- glue_jobs/silver.py
+++ glue_jobs/silver.py
@@ -1,27 +1,37 @@
-print('Reading file from s3 silver bucket')
+
 # Read file from S3
+print('Reading files from s3 Bronze buckets')
+customers_bronze=spark.read.parquet('s3://aws-s3-project-08apr2025/bronze/b_customers/')
+products_bronze=spark.read.parquet('s3://aws-s3-project-08apr2025/bronze/b_products/')
+orders_bronze=spark.read.parquet('s3://aws-s3-project-08apr2025/bronze/b_orders/')
+print('Files reading done !!!')
+# Transform Data From All Dfs and Join
+print('Joining DataFrames')
 
-gold_sales= spark.read.parquet('s3://aws-s3-project-08apr2025/silver/')
+from pyspark.sql.functions import col
 
-# Transform Data
-print('transforming the data')
-from pyspark.sql.functions import sum,year,month,col
+cu=customers_bronze.alias('cu')
+ord=orders_bronze.alias('ord')
+pr=products_bronze.alias('pr')
 
-total_revenue= gold_sales.groupBy('product_name').agg(sum('total_price').alias('total_revenue'))
-country_revenue= gold_sales.groupBy('country').agg(sum('total_price').alias('country_revenue'))
-sales_year=gold_sales\
-            .withColumn('year',year(col('order_dates')))\
-            .withColumn('month',month(col('order_dates')))
+
+sales=ord\
+      .join(cu,'customer_id','left')\
+      .join(pr,'product_id','left')
+print('Joining done! Cleaning joined dataframe now!!!')
+sales_new= sales.select(
+    'order_id','product_name',
+    'customer_id','name',
+    'category','quantity','country',
+    'price_cast','order_dates')
+
+sales_clean=sales_new.withColumnRenamed('price_cast','price')
+
+total_price=sales_clean.withColumn('total_price',col('quantity')*col('price'))
+
+print('dataframes joined and transformed')
 
 # Write transformed data back to S3
-print('writing data back to final gold s3 bucket')
-gold_sales_finalized= sales_year\
-                        .write.mode('overwrite')\
-                        .partitionBy('year','month')\
-                        .parquet('s3://aws-s3-project-08apr2025/gold/')
-revenue_per_product=total_revenue\
-                        .write.mode('overwrite')\
-                        .parquet('s3://aws-s3-project-08apr2025/gold/revenue_per_product/')
-revenue_per_country=country_revenue\
-                        .write.mode('overwrite')\
-                        .parquet('s3://aws-s3-project-08apr2025/gold/revenue_per_country/')
+print('writing joined and transformed data back to s3 silver bucket')
+final_price=total_price.write.mode('overwrite').parquet('s3://aws-s3-project-08apr2025/silver/')
+# ----------------------------
